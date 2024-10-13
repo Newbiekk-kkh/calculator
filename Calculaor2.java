6d6cd42742a98a16f7eda183daf912f4f102ba34
newbiekk <newbiekk1126@gmail.com>
2024-10-14 00:44:14 +0900

    입력유효성검사

--- Calculaor2.java
+++ Calculaor2.java
@@ -10,86 +10,65 @@ public class Calculaor2 {
         String repeat = "네";
 
         while (Objects.equals(repeat, "네")) {
-                System.out.println("계산하고 싶은 첫번째 양의 정수를 입력해주세요.");
-                int inputNumber1 = sc.nextInt();
-                sc.nextLine();
+            System.out.println("계산하고 싶은 첫번째 양의 정수를 입력해주세요.");
+            int inputNumber1 = sc.nextInt();
+            sc.nextLine();
 
-                // 두번째 숫자 입력
-                System.out.println("계산하고 싶은 두번째 양의 정수를 입력해주세요.");
-                int inputNumber2 = sc.nextInt();
-                sc.nextLine();
+            // 두번째 숫자 입력
+            System.out.println("계산하고 싶은 두번째 양의 정수를 입력해주세요.");
+            int inputNumber2 = sc.nextInt();
+            sc.nextLine();
 
-                // 사칙연산 기호 입력
-                System.out.println("계산하고 싶은 사칙연산 기호를 입력해주세요.");
-                String inputOperation = sc.nextLine();
+            // 사칙연산 기호 입력
+            System.out.println("계산하고 싶은 사칙연산 기호를 입력해주세요.");
+            String inputOperation = sc.nextLine();
 
-                switch (inputOperation) {
-                    case "+":
-                        result = inputNumber1 + inputNumber2;
-                        System.out.println("결과값: " + result);
+            switch (inputOperation) {
+                case "+":
+                    result = inputNumber1 + inputNumber2;
+                    System.out.println("결과값: " + result);
+                    break;
 
-                        while (Objects.equals(repeat, "네") || Objects.equals(repeat, "아니오")) {
-                            System.out.println("더 계산하시겠습니까? (네 or 아니오)");
-                            repeat = sc.nextLine();
-                            System.out.println("잘못된 입력입니다. '네' 또는 '아니오'를 입력해주세요. ");
-                            repeat = sc.nextLine();
-                        }
+                case "-":
+                    result = inputNumber1 - inputNumber2;
+                    System.out.println("결과값: " + result);
+                    break;
 
-                        break;
+                case "*":
+                    result = inputNumber1 * inputNumber2;
+                    System.out.println("결과값: " + result);
+                    break;
 
-                    case "-":
-                        result = inputNumber1 - inputNumber2;
-                        System.out.println("결과값: " + result);
+                case "/":
+                    if (inputNumber2 != 0) {
+                        // 나누기에 소숫점까지 보여주기위한 형변환
+                        double input1 = (double) inputNumber1;
+                        double input2 = (double) inputNumber2;
+                        double doubleResult;
+                        doubleResult = input1 / input2;
 
-                        while (Objects.equals(repeat, "네") || Objects.equals(repeat, "아니오")) {
-                            System.out.println("더 계산하시겠습니까? (네 or 아니오)");
-                            repeat = sc.nextLine();
-                            System.out.println("잘못된 입력입니다. '네' 또는 '아니오'를 입력해주세요. ");
-                            repeat = sc.nextLine();
-                        }
+                        System.out.println("결과값: " + doubleResult);
+                    } else if (inputNumber2 == 0) {
+                        System.out.println("나눗셈 연산에서 분모에 0이 입력될 수 없습니다.");
+                    }
+                    break;
 
-                        break;
+                default:
+                    System.out.println("잘못된 연산기호를 입력하셨습니다.");
+                    break;
+            }
 
-                    case "*":
-                        result = inputNumber1 * inputNumber2;
-                        System.out.println("결과값: " + result);
+            System.out.println("다시 계산하시겠습니까? '네' 또는 '아니오'를 입력해주세요.");
+            repeat = sc.nextLine();
 
-                        while (Objects.equals(repeat, "네") || Objects.equals(repeat, "아니오")) {
-                            System.out.println("더 계산하시겠습니까? (네 or 아니오)");
-                            repeat = sc.nextLine();
-                            System.out.println("잘못된 입력입니다. '네' 또는 '아니오'를 입력해주세요. ");
-                            repeat = sc.nextLine();
-                        }
-
-                        break;
-
-                    case "/":
-                        if (inputNumber2 != 0) {
-                            // 나누기에 소숫점까지 보여주기위한 형변환
-                            double input1 = (double) inputNumber1;
-                            double input2 = (double) inputNumber2;
-                            double doubleResult;
-                            doubleResult = input1 / input2;
-
-                            System.out.println("결과값: " + doubleResult);
-                        } else if (inputNumber2 == 0) {
-                            System.out.println("나눗셈 연산에서 분모에 0이 입력될 수 없습니다.");
-                        }
-
-                        while (Objects.equals(repeat, "네") || Objects.equals(repeat, "아니오")) {
-                            System.out.println("더 계산하시겠습니까? (네 or 아니오)");
-                            repeat = sc.nextLine();
-                            System.out.println("잘못된 입력입니다. '네' 또는 '아니오'를 입력해주세요. ");
-                            repeat = sc.nextLine();
-                        }
-                        break;
-
-                    default:
-                        System.out.println("잘못된 연산기호를 입력하셨습니다.");
-                        System.out.println("다시 계산하시겠습니까? (네 or 아니오)");
-                        repeat = sc.nextLine();
-                        break;
+            while (true) {
+                if (Objects.equals(repeat, "네") || Objects.equals(repeat, "아니오")) {
+                    break;
+                } else {
+                    System.out.println("잘못된 입력입니다. '네' 또는 '아니오'를 입력해주세요.");
+                    repeat = sc.nextLine();
                 }
+            }
         }
         System.out.println("계산기를 종료합니다.");
     }
