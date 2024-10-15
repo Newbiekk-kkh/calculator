d23c1ec1c3bde6000e9053bcd2bd1d4994b9ab74
newbiekk <newbiekk1126@gmail.com>
2024-10-15 18:28:34 +0900

    docs : "주석을 달아서 코드 설명 추가"
    각 코드마다 주석을 달아서 기능 설명을 추가했습니다.

--- src/calculator/lv1/App.java
+++ src/calculator/lv1/App.java
@@ -3,13 +3,16 @@ package calculator.src.calculator.lv1;
 import java.util.Objects;
 import java.util.Scanner;
 
-public class Calculator {
+public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int result = 0;
-        String repeat = "네";
+        String repeat = "네";  // repeat 을 "네"로 설정해주어야 다음의 반복문이 작동
 
+
+        //계산 반복을 위해 while문을 사용
         while (Objects.equals(repeat, "네")) {
+            //첫번째 숫자 입력
             int inputNumber1;
             while (true) {
                 System.out.println("계산하고 싶은 첫번째 양의 정수를 입력해주세요.");
@@ -41,22 +44,24 @@ public class Calculator {
             System.out.println("계산하고 싶은 사칙연산 기호를 입력해주세요.");
             String inputOperation = sc.nextLine();
 
+            // 입력 받은 사칙연산 기호에 따라 계산식 작성 (switch 문으로 케이스별 작성)
             switch (inputOperation) {
+                // 더하기
                 case "+":
                     result = inputNumber1 + inputNumber2;
                     System.out.println("결과값: " + result);
                     break;
-
+                // 빼기
                 case "-":
                     result = inputNumber1 - inputNumber2;
                     System.out.println("결과값: " + result);
                     break;
-
+                // 곱하기
                 case "*":
                     result = inputNumber1 * inputNumber2;
                     System.out.println("결과값: " + result);
                     break;
-
+                // 나누기
                 case "/":
                     if (inputNumber2 != 0) {
                         // 나누기에 소숫점까지 보여주기위한 형변환
@@ -70,7 +75,7 @@ public class Calculator {
                         System.out.println("나눗셈 연산에서 분모에 0이 입력될 수 없습니다.");
                     }
                     break;
-
+                // +, -, *, / 가 아닌 다른입력을 받았을때
                 default:
                     System.out.println("잘못된 연산기호를 입력하셨습니다.");
                     break;
@@ -79,6 +84,7 @@ public class Calculator {
             System.out.println("다시 계산하시겠습니까? '네' 또는 '아니오'를 입력해주세요.");
             repeat = sc.nextLine();
 
+            // repeat 에 잘못된 값이 입력되었을시 "네" 또는 "아니오" 가 입력될때 까지 반복
             while (true) {
                 if (Objects.equals(repeat, "네") || Objects.equals(repeat, "아니오")) {
                     break;
